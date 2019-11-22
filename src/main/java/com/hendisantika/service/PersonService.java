38393cef5dfca6818f834d0988da6b18b48243e5
Hendi Santika <hendisantika@gmail.com>
2019-11-23 06:21:39 +0700

    #15 - Adjust with Spring Boot 2.2.0.RELEASE version

--- src/main/java/com/hendisantika/service/PersonService.java
+++ src/main/java/com/hendisantika/service/PersonService.java
@@ -6,6 +6,8 @@ import com.hendisantika.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 /**
  * Created by ekansh on 13/7/15.
  */
@@ -22,8 +24,8 @@ public class PersonService {
         return personRepository.findAll();
     }
 
-    public Person findById(Long id){
-        return personRepository.findOne(id);
+    public Optional<Person> findById(Long id) {
+        return personRepository.findById(id);
     }
 
     public Person save(Person person){
